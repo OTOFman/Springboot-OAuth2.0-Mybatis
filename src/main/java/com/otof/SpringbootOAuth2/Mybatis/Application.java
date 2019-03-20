3738f5ec00a8d6dc52aafbbc077e01fe2a8d2fad
OTOFman <zryu@1314-edu.com>
2019-03-20 12:06:21 +0800

    build springboot OAuth2.0 mybatis

---
+++ src/main/java/com/otof/SpringbootOAuth2/Mybatis/Application.java
@@ -0,0 +1,13 @@
+package com.otof.SpringbootOAuth2.Mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
